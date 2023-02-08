bb36e4161f1c2ca7d2bfbf9fe448c0076d454751
John Doe <99072495@mydavinci.nl>
2023-02-08 15:27:35 +0100

    update

---
+++ module 1/m01o1/m01o1.js
@@ -0,0 +1,16 @@
+let uitleg = "In een parkeergarage passen 80 auto's op de begane grond en 120 op een verdieping.\n"+
+"Vraag het gewenste aantal auto's in de garage en bereken het aantal verdiepingen wat je nodig hebt.";
+
+alert(uitleg);
+let gewenste_aantal_autos = prompt("Hoeveel autos wilt u kwijt in de parkeergarage?");
+// Berekenen hier het aantal verdiepingen
+if (gewenste_aantal_autos <= 80){
+    antwoord = 0
+} else if (gewenste_aantal_autos > 80){
+    antwoord = Math.ceil((gewenste_aantal_autos - 80) / 120)
+}
+
+document.getElementById("antwoord").innerText = ("Om het gewenste aantal autos kwijt te kunnen heb ik " + antwoord + " verdiepingen nodig.");
+
+console.log("Om het gewenste aantal autos kwijt te kunnen heb ik " + antwoord + " verdiepingen nodig.")
+alert("Om het gewenste aantal autos kwijt te kunnen heb ik " + antwoord + " verdiepingen nodig.");
