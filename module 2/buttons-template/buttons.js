9607b65ff502cfbf92dbd3bc0eca1a3ab22a4141
John Doe <99072495@mydavinci.nl>
2023-04-08 02:27:00 +0200

    buttons

--- module 2/buttons-template/buttons.js
+++ module 2/buttons-template/buttons.js
@@ -1,5 +1,29 @@
+const img = document.getElementById('image1');
+const img2 = document.getElementById('image2');
+var button1 = 0
+var button2 = 0
+var button3 = 0
 
-a==true
-while(a = true){
+
+
+function changeImage1(){
+
+  img.src = 'images/bg1.jpg';
+  img2.src = 'images/1.jpg';
+  button1 +=1
+  document.getElementById('button_1').innerHTML = button1
     
 }
+function changeImage2(){
+  img.src = 'images/bg2.jpg';
+  img2.src = 'images/2.jpg';
+  button2 +=1
+  document.getElementById('button_2').innerHTML = button2
+}
+function changeImage3(){
+  img.src = 'images/bg3.jpg';
+  img2.src = 'images/3.jpg';
+  button3 +=1
+  document.getElementById('button_3').innerHTML = button3
+}
+
