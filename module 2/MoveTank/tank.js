2c8e922ed3b727cb3071f2b7451e0b1106a0f079
John Doe <99072495@mydavinci.nl>
2023-06-01 14:22:20 +0200

    tank_update

--- module 2/MoveTank/tank.js
+++ module 2/MoveTank/tank.js
@@ -1,13 +1,25 @@
 var image = document.getElementById("image");
-
+var demo = document.getElementById("demo")
 document.onkeydown = checkKey;
 image.style.transform = "rotate(90deg)"
+var width = window.innerWidth - 100;
+var height = window.innerHeight - 100;
+
 var position = 0
 var extra = 0
-var marginn = 0
+var marginn = 5
+var leftorright = 5
+
 function checkKey(e) {
-    position = position + 164 + extra
-    marginn = marginn + 2
+    position = position + 164
+    // console.log(marginn)
+    // console.log(leftorright)
+    // console.log(position)
+    console.log('height = ',height)
+    console.log('width = ',width)
+    console.log('marginn/updown = ',marginn)
+    console.log('leftorright = ',leftorright)
+    console.log('position = ',position)
 
 	console.log("key nr = " + e.keyCode);
     e = e || window.event;
@@ -15,21 +27,55 @@ function checkKey(e) {
     	console.log("spacebar");
     } else if (e.keyCode == '38') {  // up arrow
         console.log("Up arrow");
+        image.style.transform = "rotate(360deg)"
+        image.style.backgroundPosition = `-${parseInt(position)}px 0px`;
+        marginn = marginn -5;
+        demo.style.top = `${marginn}px`;
+        if (marginn == 0){
+            marginn = 575
+        }
+
     } else if (e.keyCode == '40') { // down arrow
         console.log("down arrow");
+        marginn = marginn + 5
+        image.style.transform = "rotate(180deg)"
+        image.style.backgroundPosition = `-${parseInt(position)}px 0px`;
+        demo.style.top =`${parseInt(marginn)}px`;
+        if (marginn == 575){
+            marginn = 0
+        }
+
     } else if (e.keyCode == '37') { // left arrow
+        leftorright = leftorright - 5
     	console.log("left arrow");
         image.style.transform = "rotate(-90deg)"
         image.style.backgroundPosition = `${position}px 0px`;
+        image.style.left = `${leftorright}px`;
+        if (leftorright == 0){
+            leftorright = 1300
+        };
+
     } else if (e.keyCode == '39') {   // right arrow
-        image.style.transform = "rotate(90deg)"
+        leftorright = leftorright + 5
+        image.style.transform = "rotate(90deg)";
     	console.log("right arrow");
     	image.style.backgroundPosition = `${position}px 0px`; // check goed de rupsband
-        image.style.margin = `${marginn}px`
-    }
-    console.log(position)
-    extra = extra + 2
+        image.style.left = `${leftorright}px`;
+        if (leftorright > 1299){
+            leftorright = 0
+        }
     
+    }
+    
+    if (position < 1312) { 
+        position = position + 256;
+    } else { 
+        position = 256; 
+    } 
+
+
+
+    console.log(leftorright)
 }
 // function animateScript() {
 //     document.getElementById("image").style.backgroundPosition = 
