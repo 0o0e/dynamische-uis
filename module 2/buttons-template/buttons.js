478d2f8c6ff047caec9a71bc1b325a1fba0746a4
John Doe <99072495@mydavinci.nl>
2023-06-15 01:57:30 +0200

    updates

--- module 2/buttons-template/buttons.js
+++ module 2/buttons-template/buttons.js
@@ -1,21 +1,26 @@
 const img = document.getElementById('image1');
 const img2 = document.getElementById('image2');
 
+
+
 var button1 = 0
 var button2 = 0
 var button3 = 0
 
+button_1.onclick = changeImage1
+button_2.onclick = changeImage2
+button_3.onclick = changeImage3
 
-
-function changeImage1(clicked_id){
+function changeImage1(){
+  clicked_id = this.id
   img.src = 'images/bg1.jpg';
   img2.src = 'images/1.jpg';
   button1 +=1
   document.getElementById('button_1').innerHTML = button1
   if (clicked_id == 'button_1'){
-    document.querySelector('#button_1').disabled = true;
-    document.querySelector('#button_2').disabled = false;
-    document.querySelector('#button_3').disabled = false;
+    button_1.disabled = true;
+    button_2.disabled = false;
+    button_3.disabled = false;
 
     document.getElementById('button_1').style.backgroundColor='#911';
     document.getElementById('button_2').style.backgroundColor='#00cd00';
@@ -24,34 +29,34 @@ function changeImage1(clicked_id){
   }
 }
 
-function changeImage2(clicked_id){
+function changeImage2(){
+  clicked_id = this.id.replace("button_","");
+  
   img.src = 'images/bg2.jpg';
   img2.src = 'images/2.jpg';
   button2 +=1
   document.getElementById('button_2').innerHTML = button2
   if (clicked_id == 'button_2'){
-    document.querySelector('#button_1').disabled = false;
-    document.querySelector('#button_2').disabled = true;
-    document.querySelector('#button_3').disabled = false;
+    button_1.disabled = false;
+    button_2.disabled = true;
+    button_3.disabled = false;
 
     document.getElementById('button_1').style.backgroundColor='#00cd00';
     document.getElementById('button_2').style.backgroundColor='#911';
-    document.getElementById('button_3').style.backgroundColor='#00cd00';
-
-
-    
+    document.getElementById('button_3').style.backgroundColor='#00cd00';    
   }
 }
 
-function changeImage3(clicked_id){
+function changeImage3(){
+  clicked_id = this.id
   img.src = 'images/bg3.jpg';
   img2.src = 'images/3.jpg';
   button3 +=1
   document.getElementById('button_3').innerHTML = button3
   if (clicked_id == 'button_3'){
-    document.querySelector('#button_1').disabled = false;
-    document.querySelector('#button_2').disabled = false;
-    document.querySelector('#button_3').disabled = true;
+    button_1.disabled = false;
+    button_2.disabled = false;
+    button_3.disabled = true;
 
     
     document.getElementById('button_1').style.backgroundColor='#00cd00';
